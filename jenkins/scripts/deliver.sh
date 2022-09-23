7d0e2a0662fbb7ec1d28f34c5d96454938042539
diditdwipermadi <diditdwipermadi@gmail.com>
2022-09-23 16:28:12 +0700

    Update Jenkins mvn

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -8,19 +8,7 @@ set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
-echo 'The following complex command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | tail -1`
-set +x
-
-echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
-set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-set +x
-
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar target/my-app-1.0-SNAPSHOT.jar
